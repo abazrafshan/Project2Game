8d9357d852f3a6f053818d92018fa8c971f75f02
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-06-10 15:38:20 -0700

    help me

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
     app.get("/game", function(req, res) {
+        db.sequelize.sync({force:true}).then(function() {
         db.sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
         db.sequelize.query('DELETE FROM Property WHERE ID > 0');
         db.sequelize.query('ALTER TABLE Property AUTO_INCREMENT = 1');
@@ -33,6 +34,7 @@ module.exports = function(app) {
          db.sequelize.query('INSERT INTO Property (name, price, payout) VALUES ("Santa Monica Pier", 360, 72)');
          db.sequelize.query('INSERT INTO Property (name, price, payout) VALUES ("Murphy Way", 380, 76)');
          db.sequelize.query('INSERT INTO Property (name) VALUES ("Collect Your Rent")');
+        })
         res.render("index");
     })
 }
